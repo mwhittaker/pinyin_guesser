d458e87adb24ccbfe20edde53e035329fc280314
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-08-29 17:22:00 -0700

    First cut at interactive pinyin guesser.

--- pinyin.js
+++ pinyin.js
@@ -1,11 +1,12 @@
-function guess_pinyin() {
-  const allowed = new Set([
-  //      a    o    e    i     u     u
-         "a", "o", "e", "yi", "wu", "yu",
-  /*b*/  "ba",
-  /*p*/  "pa",
-  /*m*/  "ma",
-  /*f*/  "fa",
+function random_pinyin() {
+  // The set of pinyin we want to guess from.
+  const pinyins = [
+  //      a     o     e    i     u     u
+         "a",  "o",  "e", "yi", "wu", "yu",
+  /*b*/  "ba", "bo",
+  /*p*/  "pa", "po",
+  /*m*/  "ma", "mo", "me",
+  /*f*/  "fa", "fo",
 
   /*d*/  "da",
   /*t*/  "ta",
@@ -28,15 +29,63 @@ function guess_pinyin() {
   /*j*/  "cha",
   /*q*/  "sha",
   /*x*/  "ra",
-  ]);
-  const nodes = Array.from(document.querySelectorAll(".val"))
-                     .filter((node) => allowed.has(node.innerHTML));
-  const node = nodes[Math.floor(Math.random() * nodes.length)];
-  const tone = Math.floor(Math.random() * 4);
-  const sound_player = document.getElementById("snd_player");
+  ];
 
-  node.click();
-  sound_player.children[tone].click();
-
-  return node.innerHTML + " tone " + tone;
+  return {
+    pinyin: pinyins[Math.floor(Math.random() * pinyins.length)],
+    tone: Math.floor(Math.random() * 4),
+  };
 }
+
+function pinyin_to_string(pinyin) {
+  return pinyin === null ? "null" : `${pinyin.pinyin} tone ${pinyin.tone + 1}`;
+}
+
+function update_ui(last_pinyin, current_pinyin) {
+  document.getElementById("last_pinyin").innerHTML =
+    "last pinyin was " + pinyin_to_string(last_pinyin);
+  document.getElementById("current_pinyin").innerHTML =
+    pinyin_to_string(current_pinyin);
+  document.getElementById("detail_pinyin").removeAttribute("open");
+}
+
+function play_pinyin(pinyin) {
+  const node = Array.from(document.querySelectorAll(".val"))
+                    .filter((node) => node.innerHTML === pinyin.pinyin)[0];
+  node.click()
+  document.getElementById("snd_player").children[pinyin.tone].click()
+  document.getElementById("pinyin_chart").click()
+}
+
+function main() {
+  // Set up our HTML.
+  const title = document.getElementById("pinyin_chart");
+  title.innerHTML += `
+    <center>
+    <button id="next_pinyin">Next</button>
+    <button id="play_pinyin">Play</button>
+    <span id="last_pinyin">span</span>
+    <details id="detail_pinyin">
+      <summary>Click to see answer</summary>
+      <span id="current_pinyin"></span>
+    </details>
+    </center>
+  `
+
+  let last_pinyin = null;
+  let current_pinyin = random_pinyin();
+  update_ui(last_pinyin, current_pinyin);
+
+  document.getElementById("play_pinyin").onclick = () => {
+    play_pinyin(current_pinyin);
+  };
+
+  document.getElementById("next_pinyin").onclick = () => {
+    last_pinyin = current_pinyin;
+    current_pinyin = random_pinyin();
+    update_ui(last_pinyin, current_pinyin);
+    play_pinyin(current_pinyin);
+  };
+}
+
+main();
